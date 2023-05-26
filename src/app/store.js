e7093ea57429ceb9b960a2a610710c8312f8c8ab
Suliman <sssuliman@bk.ru>
2023-05-27 02:29:59 +0300

    Сделал заявку на приобретение товара

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/applicationSlice";
 import productSlice from "../features/productSlice";
 import categorySlice from "../features/categorySlice";
+import request from "features/requestSlice";
 
 export const store = configureStore({
   reducer: {
     application,
     productSlice,
     categorySlice,
+    request,
   },
 });
