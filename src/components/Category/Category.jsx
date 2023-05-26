f8d1675ce187851e3069e3da0deaa84ffc8067e5
Suliman <sssuliman@bk.ru>
2023-05-26 13:35:23 +0300

    mainPage

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./category.module.scss";
+const Category = () => {
+  return (<div className={styles.category}>
+    
+  </div>);
+};
+
+export default Category;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import styles from "./main.module.scss";
 import SliderList from "../../components/SliderList/SliderList";
+import cow from "../../icons/cow.svg";
+import koko from "../../icons/koko.svg";
+import heart from "../../icons/heart.svg";
+import product from "../../icons/product.svg";
 
 const Main = () => {
   return (
-    <div>
+    <>
       <SliderList />
-    </div>
+      <div className={styles.infoBlock}>
+        <h3>Promise — сервис для предзаказа настоящих фермерских продуктов</h3>
+        <div className={styles.blocks}>
+          <div>
+            <img src={cow} alt="№" />
+            <span>200 фермерских хозяйств</span>
+          </div>
+          <div>
+            <img src={product} alt="№" />
+            <span>200 фермерских продуктов</span>
+          </div>
+          <div>
+            <img src={koko} alt="№" />
+            <span>Контролируем качество от грядки до тарелки</span>
+          </div>
+          <div>
+            <img src={heart} alt="№" />
+            <span>Природный вкус в каждом продукте</span>
+          </div>
+        </div>
+        <div className={styles.bg}></div>
+      </div>
+    </>
   );
 };
 
