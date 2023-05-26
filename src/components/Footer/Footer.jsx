9b8f24f0dcab17a286cb3794252c48c63f540ccf
MensoMG <m.massanino@gmail.com>
2023-05-26 21:34:52 +0300

    precomplete profile components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export default function App() {
   return (
       <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
         <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-
           <div>
-            <a href='' className='me-4 text-reset'>
-              <MDBIcon fab icon="facebook-f" />
-            </a>
             <a href='' className='me-4 text-reset'>
               <MDBIcon fab icon="twitter" />
             </a>
@@ -18,12 +15,6 @@ export default function App() {
             </a>
             <a href='' className='me-4 text-reset'>
               <MDBIcon fab icon="instagram" />
-            </a>
-            <a href='' className='me-4 text-reset'>
-              <MDBIcon fab icon="linkedin" />
-            </a>
-            <a href='' className='me-4 text-reset'>
-              <MDBIcon fab icon="github" />
             </a>
           </div>
         </section>
@@ -113,9 +104,7 @@ export default function App() {
 
         <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
           © 2021 Copyright:
-          <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-            MDBootstrap.com
-          </a>
+          <a className='text-reset fw-bold' href='https://test.test'>Promise Shop</a>
         </div>
       </MDBFooter>
   );
