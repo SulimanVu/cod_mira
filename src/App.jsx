57d06a766c1986d9f4855cb343cc80e2c7834f68
Aizzzen <gadamurrr@gmail.com>
2023-05-26 23:07:12 +0300

    catalog page yunus

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Bascket from "./pages/Bascket/Bascket";
 import About from "./pages/About/About";
+import Category from "pages/Category/Category";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/bascket/:id" element={<Bascket />} />
         <Route path="/about" element={<About />} />
+        <Route path="/categories" element={<Category />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Product/ProductPage.jsx
+++ src/components/Product/ProductPage.jsx
@@ -3,6 +3,7 @@ import { ProductCard } from "./ProductCard/ProductCard"
 import styles from "./ProductPage.module.scss"
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCategory } from 'features/categorySlice';
+import { Link } from 'react-router-dom';
 
 
 const FEATURES = [
@@ -16,20 +17,25 @@ const FEATURES = [
 
 
 export const ProductPage = () => {
-    const categories = useSelector((state) => state.categorySlice.categories)
+    let categories = useSelector((state) => state.categorySlice.categories)
     console.log(categories);
     
+    // if(categories.length >=3 ) categories = categories.slice(2)
+
     return (
-        <div className={styles.productTitle}>
-            <h2>НАШ АССОРТИМЕНТ</h2>
-            <div className={styles.cards}>
-                {categories.map((obj) =>
+        <div style={{width: "80%", margin: 'auto'}}>
+            <h2 className={styles.productTitle}>НАШ АССОРТИМЕНТ</h2>
+            <div >
+                {categories.map((obj, i) =>
                     <>
-                        <div>{obj.name}</div>
-                        {obj.products.map(item => <ProductCard img={item.image} />)}
+                        <div className={styles.categoryName}>{obj.name} <br/><i style={{ marginTop: 50, cursor: 'pointer'}}>перейти ➔</i></div>
+                        <div className={styles.cards}>
+                            {obj.products.map(item => <ProductCard img={item.image} title={item.name} />)}
+                        </div>
                     </>
                 )}
             </div>
+            <Link to="/categories"><p style={{cursor: 'pointer'}}>перейти к каталогу товаров</p></Link>
         </div>
     );
 };
