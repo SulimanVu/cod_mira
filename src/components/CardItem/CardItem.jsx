47fd557180dc2ce8ae51605182154d32c8814c47
Suliman <sssuliman@bk.ru>
2023-05-26 23:05:36 +0300

    fix carditem

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./cardItem.module.scss";
+import { addToBascket } from "features/applicationSlice";
 
 const CardItem = ({ image, description, price, _id }) => {
   const dispatch = useDispatch();
   const [like, setLike] = useState();
 
   const token = localStorage.getItem("token");
+  const id = localStorage.getItem("id");
 
   const handleLike = (e) => {
     e.preventDefault();
     setLike(!like);
-    
+    dispatch(addToBascket({ id, bascket: _id }));
   };
 
   return (
