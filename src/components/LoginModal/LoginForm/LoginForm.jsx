5ef2e32ff819d7d818850bb069ee4234bf78177e
Suliman <sssuliman@bk.ru>
2023-05-27 09:42:36 +0300

    Убрал консоли

--- src/components/LoginModal/LoginForm/LoginForm.jsx
+++ src/components/LoginModal/LoginForm/LoginForm.jsx
@@ -1,90 +1,89 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import   {loginThunk}   from "features/applicationSlice";
-import styles from './LoginForm.module.scss'
+import { loginThunk } from "features/applicationSlice";
+import styles from "./LoginForm.module.scss";
 
 import { ToastContainer, toast } from "react-toastify";
 
+export const LoginForm = ({ activeLogin, setActiveLogin }) => {
+  const dispatch = useDispatch();
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const error = useSelector((state) => state.application.error);
+  const token = useSelector((state) => state.application.token);
 
-
-
-export const LoginForm = ({activeLogin, setActiveLogin}) => {
-    console.log(localStorage.getItem('id'))
-
-    const dispatch = useDispatch()
-    const [login, setLogin] = useState('')
-    const [password, setPassword] = useState('')
-    const error = useSelector((state)=> state.application.error)
-    const token = useSelector((state)=> state.application.token)
-
-    useEffect(()=>{
-        if(token){
-            setActiveLogin(false)
-        }
-    },[token])
-
-    const Toaster = (text) => {
-      toast.error(text,);
-    };
-
-    const handleValidation = ({ login, password }) => {
-      if (password === "") {
-        Toaster("Пароль - обьязательное поле!");
-        return false;
-      } else if (login === '') {
-        Toaster("Логин - обьязательное поле!");
-        return false;
-      } else if (password.length < 8) {
-        Toaster("Пароль не должен быть меньше 8 символов!");
-        return false;
-      } 
-      return true
-    };
-
-    const handleLogin = (e,{login, password}) => {
-      e.preventDefault();
-      if(handleValidation({login,password})) {
-        dispatch(loginThunk({login, password}))
-        setLogin('')
-        setPassword('')
-      }
+  useEffect(() => {
+    if (token) {
+      setActiveLogin(false);
     }
+  }, [token]);
 
-    const handleForm = (e) => {
-      e.preventDefault()
-      e.stopPropagation()
+  const Toaster = (text) => {
+    toast.error(text);
+  };
+
+  const handleValidation = ({ login, password }) => {
+    if (password === "") {
+      Toaster("Пароль - обьязательное поле!");
+      return false;
+    } else if (login === "") {
+      Toaster("Логин - обьязательное поле!");
+      return false;
+    } else if (password.length < 8) {
+      Toaster("Пароль не должен быть меньше 8 символов!");
+      return false;
     }
+    return true;
+  };
+
+  const handleLogin = (e, { login, password }) => {
+    e.preventDefault();
+    if (handleValidation({ login, password })) {
+      dispatch(loginThunk({ login, password }));
+      setLogin("");
+      setPassword("");
+    }
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
 
   return (
     <>
-    <ToastContainer />
-    <div
-      className={activeLogin ? styles.nomodal : styles.modal}
-      onClick={() => setActiveLogin(false)}
-    >
-      <form className={styles.content} onClick={(e) => handleForm(e)}>
-        <h2>Войти</h2>
-        <input
-          type="text"
-          placeholder="Введите логин"
-          className={styles.input}
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Введите пароль"
-          className={styles.input}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && <div className={styles.error}>Неправильный логин или пароль</div>  }
-        <button onClick={(e) => handleLogin(e, {login, password})} className={styles.btn}>
-          Войти
-        </button>
-      </form>
-    </div>
+      <ToastContainer />
+      <div
+        className={activeLogin ? styles.nomodal : styles.modal}
+        onClick={() => setActiveLogin(false)}
+      >
+        <form className={styles.content} onClick={(e) => handleForm(e)}>
+          <h2>Войти</h2>
+          <input
+            type="text"
+            placeholder="Введите логин"
+            className={styles.input}
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Введите пароль"
+            className={styles.input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && (
+            <div className={styles.error}>Неправильный логин или пароль</div>
+          )}
+          <button
+            onClick={(e) => handleLogin(e, { login, password })}
+            className={styles.btn}
+          >
+            Войти
+          </button>
+        </form>
+      </div>
     </>
   );
 };
-;
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -7,40 +7,35 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
 const Category = () => {
-    const {id} = useParams()
-    const dispatch = useDispatch()
-    const categories = useSelector((state) => state.categorySlice.categories)
-    const currentProducts = useSelector((state) =>
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categorySlice.categories);
+  const currentProducts = useSelector((state) =>
     state.categorySlice.categories.find((item) => item._id == id)
   );
-    
-    console.log(currentProducts.products);
 
+  useEffect(() => {
+    dispatch(fetchCategory());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchCategory())
-    }, [dispatch, ])
-
-    return (
-        <div className={styles.container}>
-            <section className={styles.categories}>
-                <div className={styles.select}>
-                    {categories?.map((obj, i) =>
-                        <Link className={styles.link} to={"/categories/" + obj._id}>
-                            <div 
-                            className={styles.selectItem}
-                            >{obj.name}</div>
-                        </Link>
-                    )}
-                </div>
-            </section>
-            <section className={styles.categoryItems}>
-            <div>
-                <CategoryCardsPage products={currentProducts.products}/>
-            </div>
-            </section>
+  return (
+    <div className={styles.container}>
+      <section className={styles.categories}>
+        <div className={styles.select}>
+          {categories?.map((obj, i) => (
+            <Link className={styles.link} to={"/categories/" + obj._id}>
+              <div className={styles.selectItem}>{obj.name}</div>
+            </Link>
+          ))}
         </div>
-      );
+      </section>
+      <section className={styles.categoryItems}>
+        <div>
+          <CategoryCardsPage products={currentProducts.products} />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Category;
